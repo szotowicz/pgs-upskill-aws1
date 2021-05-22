425f253d3d27b788f5c3cea033c379e69ecfa5b7
Mikolaj Szotowicz <mikolaj.szotowicz@gmail.com>
2021-05-22 09:49:07 +0200

    Create server app template

--- ups-server/app.js
+++ ups-server/app.js
@@ -2,8 +2,22 @@ const express = require('express')
 const app = express()
 const port = 3001
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.get('/language', (req, res) => {
+  res.send({
+    id: 1,
+    name: 'Type Script',
+    updatedAt: (new Date()).toISOString(),
+  })
+})
+
+app.post('/language', (req, res) => {
+  const body = req.body;
+  // FIXME : add body validation
+  console.log('TODO: save in database:', body);
+  res.sendStatus(204);
 })
 
 app.listen(port, () => {
