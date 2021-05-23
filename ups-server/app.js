b66107ccff112eff599258c05b5df16758d58953
Mikolaj Szotowicz <mikolaj.szotowicz@gmail.com>
2021-05-23 13:59:15 +0200

    Implement GET and POST endpoint

--- ups-server/app.js
+++ ups-server/app.js
@@ -1,25 +1,68 @@
+const dotenv = require('dotenv')
 const express = require('express')
+const mysql = require('mysql')
+
+dotenv.config()
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+})
+
+db.connect(error => {
+  if (error) {
+    console.error('[ERROR] MySQL not connected');
+  } else {
+    console.log('MySQL connected');
+  }
+})
+
 const app = express()
-const port = 3001
+const PORT = 3001
+const RECORD_ID = 1
 
 app.use(express.json());
 app.use(express.urlencoded());
 
 app.get('/language', (req, res) => {
-  res.send({
-    id: 1,
-    name: 'Type Script',
-    updatedAt: (new Date()).toISOString(),
+  const sql = `SELECT * FROM language WHERE language.id = ${RECORD_ID}`
+  db.query(sql, (error, results) => {
+    if (error) {
+      res.status(500).send('Unexpected error');
+    }
+    if (!results || results.length === 0) {
+      res.send({
+        id: RECORD_ID,
+        name: 'Type Script',
+        updatedAt: (new Date()).toISOString(),
+      });
+    }
+    res.send(results[0]);
   })
 })
 
 app.post('/language', (req, res) => {
-  const body = req.body;
-  // FIXME : add body validation
-  console.log('TODO: save in database:', body);
-  res.sendStatus(204);
+  let updatedName = req.body.name || 'Type Script';
+  if (!(/^[A-Za-z]+$/.test(updatedName))) {
+    updatedName = 'Type Script'
+  }
+
+  const updated = {
+    id: RECORD_ID,
+    name: updatedName,
+    updatedAt: (new Date()).toISOString(),
+  }
+  const sql = `INSERT INTO language SET ? ON DUPLICATE KEY UPDATE name="${updated.name}", updatedAt="${updated.updatedAt}"`
+  db.query(sql, updated, error => {
+    if (error) {
+      res.status(500).send('Unexpected error');
+    }
+    res.sendStatus(204);
+  })
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`)
 })
