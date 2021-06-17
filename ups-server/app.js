71f47556bb0498bebe97fa5ecba8e6391e38147e
Mikolaj <mikolaj.szotowicz@gmail.com>
2021-06-17 20:06:18 +0200

    Add logger (#1)

--- ups-server/app.js
+++ ups-server/app.js
@@ -10,43 +10,68 @@ const db = mysql.createConnection({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 })
-
-db.connect(error => {
-  if (error) {
-    console.error('[ERROR] MySQL not connected');
-  } else {
-    console.log('MySQL connected');
-  }
-})
-
+const TABLE_NAME = process.env.DB_TABLE_NAME
 const app = express()
 const PORT = 3001
 const RECORD_ID = 1
 
-app.use(express.json());
-app.use(express.urlencoded());
+db.connect(error => {
+  if (error) {
+    console.error(`[${(new Date()).toISOString()}][ERROR] MySQL not connected. Details: ${error}`)
+  } else {
+    console.log(`[${(new Date()).toISOString()}] MySQL connected`)
+    const checkTableSql = `SELECT * FROM information_schema.tables WHERE table_schema = '${process.env.DB_NAME}' AND table_name = '${TABLE_NAME}'`
+    db.query(checkTableSql, (error, results) => {
+      if (error) {
+        console.error(`[${(new Date()).toISOString()}][ERROR] Checking if table exists failed. Details: ${error}`)
+      } else if (results.length > 0) {
+        console.log(`[${(new Date()).toISOString()}] ${TABLE_NAME} table exists`)
+      } else {
+        const createTableSql = `CREATE TABLE ${process.env.DB_NAME}.${TABLE_NAME} (
+          id int(11) NOT NULL,
+          name varchar(100) NOT NULL,
+          updatedAt varchar(45) NOT NULL,
+          PRIMARY KEY (id)
+        )`
+        db.query(createTableSql, (error) => {
+          if (error) {
+            console.error(`[${(new Date()).toISOString()}][ERROR] Creating table failed. Details: ${error}`)
+          } else {
+            console.log(`[${(new Date()).toISOString()}] ${TABLE_NAME} table has been created`)
+          }
+        })
+      }
+    })
+  }
+})
+
+app.use(express.json())
+app.use(express.urlencoded())
 
 app.get('/language', (req, res) => {
-  const sql = `SELECT * FROM language WHERE language.id = ${RECORD_ID}`
-  db.query(sql, (error, results) => {
+  const getLanguageSql = `SELECT * FROM ${TABLE_NAME} WHERE ${TABLE_NAME}.id = ${RECORD_ID}`
+  db.query(getLanguageSql, (error, results) => {
     if (error) {
-      res.status(500).send('Unexpected error');
+      console.error(`[${(new Date()).toISOString()}][ERROR] Featching language failed. Details: ${error}`)
+      res.status(500).send('Unexpected internal error')
+      return
     }
     if (!results || results.length === 0) {
       res.send({
         id: RECORD_ID,
         name: 'Type Script',
         updatedAt: (new Date()).toISOString(),
-      });
+      })
     }
-    res.send(results[0]);
+    res.send(results[0])
   })
 })
 
 app.post('/language', (req, res) => {
-  let updatedName = req.body.name || 'Type Script';
+  let updatedName = req.body.name || 'Type Script'
   if (!(/^[A-Za-z]+$/.test(updatedName))) {
     updatedName = 'Type Script'
+    console.log(`[${(new Date()).toISOString()}] Invalid updated name. Setup default name: ${updatedName}`)
   }
 
   const updated = {
@@ -54,12 +79,14 @@ app.post('/language', (req, res) => {
     name: updatedName,
     updatedAt: (new Date()).toISOString(),
   }
-  const sql = `INSERT INTO language SET ? ON DUPLICATE KEY UPDATE name="${updated.name}", updatedAt="${updated.updatedAt}"`
-  db.query(sql, updated, error => {
+  const updateLanguageSql = `INSERT INTO ${TABLE_NAME} SET ? ON DUPLICATE KEY UPDATE name="${updated.name}", updatedAt="${updated.updatedAt}"`
+  db.query(updateLanguageSql, updated, error => {
     if (error) {
-      res.status(500).send('Unexpected error');
+      console.error(`[${(new Date()).toISOString()}][ERROR] Updating language failed. Details: ${error}`)
+      res.status(500).send('Unexpected internal error')
+      return
     }
-    res.sendStatus(204);
+    res.sendStatus(204)
   })
 })
 
